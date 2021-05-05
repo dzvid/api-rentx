7b22cfc66ea0b3691cc274b95aec21d373d4a048
David <oliveiradavid.dev@gmail.com>
2021-05-04 23:25:48 -0400

    Add create category endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
+import { categoriesRoutes } from '@routes/categories.routes';
 import express from 'express';
 
 const app = express();
 const port = 3333;
 
 app.use(express.json());
+
+app.use('/categories', categoriesRoutes);
 
 app.listen(port, () => console.log(`Server is running on port ${port}.`));
