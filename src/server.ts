4830eda1713075450c36ff8e22539e9a16fa3060
David <oliveiradavid.dev@gmail.com>
2021-05-11 16:45:37 -0400

    Add swagger

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import { router } from '@routes/index';
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+
+import swaggerConfig from './swagger.json';
 
 const app = express();
 const port = 3333;
 
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerConfig));
 app.use(router);
 
 app.listen(port, () => console.log(`Server is running on port ${port}.`));
