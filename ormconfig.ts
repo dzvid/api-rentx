d353ec32b73c3502daa949f4b39230878062fd03
David <oliveiradavid.dev@gmail.com>
2021-05-13 23:21:23 -0400

    Add Categories migration

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,10 +1,14 @@
 require('dotenv/config');
 
 module.exports = {
-  type: process.env.TYPEORM_CONNECTION,
-  host: process.env.TYPEORM_HOST,
-  port: process.env.TYPEORM_PORT,
-  username: process.env.TYPEORM_USERNAME,
-  password: process.env.TYPEORM_PASSWORD,
-  database: process.env.TYPEORM_DATABASE,
+  type: process.env.DB_CONNECTION,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  migrations: ['./src/database/migrations/*.ts'],
+  cli: {
+    migrationsDir: './src/database/migrations',
+  },
 };
