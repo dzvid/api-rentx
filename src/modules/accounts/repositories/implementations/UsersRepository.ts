9eb62f4c8b9b9396c66682ed707abac634f995d1
David <oliveiradavid.dev@gmail.com>
2021-05-17 17:44:01 -0400

    Hash user password

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -22,6 +22,11 @@ class UsersRepository implements IUsersRepository {
 
     await this.respository.save(user);
   }
+
+  async findByEmail(email: string): Promise<User> {
+    const user = await this.respository.findOne({ email });
+    return user;
+  }
 }
 
 export { UsersRepository };
--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,5 +1,6 @@
 import { ICreateUserDTO } from '@modules/accounts/dtos/ICreateUserDTO';
 import { UsersRepository } from '@modules/accounts/repositories/implementations/UsersRepository';
+import { hash } from 'bcrypt';
 import { inject, injectable } from 'tsyringe';
 
 @injectable()
@@ -15,10 +16,17 @@ class CreateUserUseCase {
     password,
     driver_license,
   }: ICreateUserDTO): Promise<void> {
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+    if (userAlreadyExists) {
+      throw new Error('User already exists');
+    }
+
+    const passwordHash = await hash(password, 8);
+
     await this.usersRepository.create({
       name,
       email,
-      password,
+      password: passwordHash,
       driver_license,
     });
   }
