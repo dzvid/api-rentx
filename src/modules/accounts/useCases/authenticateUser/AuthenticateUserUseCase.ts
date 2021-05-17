ba3d4adb258af95a47ed4c3342f5e0e00f6a0f0e
David <oliveiradavid.dev@gmail.com>
2021-05-17 19:36:42 -0400

    Add JWT authentication

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,53 @@
+import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
+import { compare } from 'bcrypt';
+import { sign } from 'jsonwebtoken';
+import { inject, injectable } from 'tsyringe';
+
+import authConfig from '../../config/auth';
+
+require('dotenv/config');
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository
+  ) {}
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+    if (!user) throw new Error('Email or password incorrect!');
+
+    const passwordMatch = await compare(password, user.password);
+    if (!passwordMatch) throw new Error('Email or password incorrect!');
+
+    const token = sign({}, authConfig.secret, {
+      subject: user.id,
+      expiresIn: authConfig.expiresIn,
+    });
+
+    const tokenReturn: IResponse = {
+      token,
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+    };
+
+    return tokenReturn;
+  }
+}
+
+export { AuthenticateUserUseCase };
