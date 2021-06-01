159748ab8b32b9db0d20c9fff401bd4b0a9826eb
David <oliveiradavid.dev@gmail.com>
2021-06-01 13:46:18 -0400

    Add middleware to check if user is admin

---
+++ src/shared/infra/http/middlewares/ensureAdmin.ts
@@ -0,0 +1,23 @@
+import { UsersRepository } from '@modules/accounts/infra/repositories/UsersRepository';
+import { NextFunction, Request, Response } from 'express';
+
+import { AppError } from '@shared/errors/AppError';
+
+import { usersRoutes } from '../routes/users.routes';
+
+export async function ensureAdmin(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const { id } = request.user;
+
+  const userRepository = new UsersRepository();
+  const user = await userRepository.findById(id);
+
+  if (!user.isAdmin) {
+    throw new AppError('User is not an admin!');
+  }
+
+  return next();
+}
--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -1,11 +1,17 @@
 import { CreateSpecificationController } from '@modules/cars/useCases/createSpecification/CreateSpecificationController';
 import { Router } from 'express';
-import { ensureAuthenticated } from 'shared/infra/http/middlewares/ensureAuthenticated';
+
+import { ensureAdmin } from '@shared/infra/http/middlewares/ensureAdmin';
+import { ensureAuthenticated } from '@shared/infra/http/middlewares/ensureAuthenticated';
 
 const specificationsRoutes = Router();
 const createSpecificationController = new CreateSpecificationController();
 
-specificationsRoutes.use(ensureAuthenticated);
-specificationsRoutes.post('/', createSpecificationController.handle);
+specificationsRoutes.post(
+  '/',
+  ensureAuthenticated,
+  ensureAdmin,
+  createSpecificationController.handle
+);
 
 export { specificationsRoutes };
