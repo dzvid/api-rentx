47dae92b2e26c22bb03fda745a84ef1085beb14a
David <oliveiradavid.dev@gmail.com>
2021-08-25 15:15:48 -0400

    chore: Databse docker

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,12 +1,11 @@
 import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 import 'dotenv/config';
 
-export default async (host = 'rentx-db'): Promise<Connection> => {
+export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host: process.env.NODE_ENV === 'test' ? 'localhost' : host,
       database:
         process.env.NODE_ENV === 'test'
           ? 'rentx_test'
